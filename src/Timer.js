2e73d126d24448cc6c232a1da42e81d89ca0a509
radcleong94 <Radcleong@gmail.com>
2020-08-23 22:17:12 +0800

    update my pomodoro-clock

--- src/Timer.js
+++ src/Timer.js
@@ -1,97 +1,169 @@
 import React, { Component } from "react";
 import "./Timer.css";
 import BreakInterval from "./BreakInterval";
+import SessionLength from "./SessionLength";
 
 class Timer extends Component {
   constructor() {
     super();
     this.state = {
       isSession: true,
-      timerSecond: 0,
+      breakLength: 5,
+      sessionLength: 25,
+      timerCount: 25 * 60,
+      currentTime: "Session",
       intervalId: 0,
+      isPlaying: false,
+      isPlay: false,
     };
   }
-
-  onPlay = () => {
-    let intervalId = setInterval(this.decreaseTimer, 1000);
-    this.props.playStopTimer(true);
-    this.setState({
-      intervalId: intervalId,
+  //Break Length section================================
+  handleIncreaseBreak = () => {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength + 1,
+      };
     });
   };
 
-  decreaseTimer = () => {
-    switch (this.state.timerSecond) {
-      case 0:
-        if (this.props.timerMinute === 0) {
+  handleDecreaseBreak = () => {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength - 1,
+      };
+    });
+  };
+
+  //Session Length section================================
+  handleIncreaseSession = () => {
+    this.setState((prevState) => {
+      return {
+        sessionLength: prevState.sessionLength + 1,
+        timerCount: (prevState.sessionLength + 1) * 60,
+      };
+    });
+  };
+
+  handleDecreaseSession = () => {
+    this.setState((prevState) => {
+      return {
+        sessionLength: prevState.sessionLength - 1,
+        timerCount: (prevState.sessionLength - 1) * 60,
+      };
+    });
+  };
+
+  handleSwitch = (isSession) => {
+    if (isSession) {
+      this.setState({
+        timerCount: this.state.sessionLength * 60,
+      });
+    } else {
+      this.setState({
+        timerCount: this.state.breakLength * 60,
+      });
+    }
+  };
+
+  onResetTimer = () => {
+    this.setState({
+      timerCount: this.state.sessionLength,
+    });
+  };
+
+  onPlayStopTimer = (isPlay) => {
+    this.setState({
+      isPlay: this.state.isPlay,
+    });
+  };
+
+  handleUpdateTimerMinSec = (count) => {
+    const minute = Math.floor(count / 60);
+    let second = count % 60;
+
+    second = second < 10 ? "0" + second : second;
+    return `${minute}:${second}`;
+  };
+
+  onPlay = () => {
+    if (this.state.isPlaying) {
+      clearInterval(this.state.intervalId);
+      this.setState({
+        isPlaying: false,
+      });
+    } else {
+      this.setState({
+        isPlaying: true,
+      });
+
+      this.state.intervalId = setInterval(() => {
+        if (this.state.timerCount === 0) {
           if (this.state.isSession) {
             this.setState({
               isSession: false,
             });
-
-            this.props.sessionSwitch(this.state.isSession);
+            this.handleSwitch(this.state.isSession);
           } else {
             this.setState({
               isSession: true,
             });
-
-            this.props.sessionSwitch(this.state.isSession);
+            this.handleSwitch(this.state.isSession);
           }
         } else {
-          this.props.updateTimerMin();
           this.setState({
-            timerSecond: 59,
+            timerCount: this.state.timerCount - 1,
           });
         }
-        break;
-      default:
-        this.setState((prevState) => {
-          return {
-            timerSecond: prevState.timerSecond - 1,
-          };
-        });
+      }, 1000);
     }
   };
 
-  onStop = () => {
-    clearInterval(this.state.intervalId);
-    this.props.playStopTimer(false);
-  };
-
   onReset = () => {
-    this.onStop();
-    this.props.resetTimer();
-    this.props.playStopTimer(false);
+    clearInterval(this.state.intervalId);
+
     this.setState({
-      timerSecond: 0,
+      timerCount: 25 * 60,
       isSession: true,
     });
   };
 
   render() {
     return (
-      <div>
+      <div className="main">
+        <h2>~Pomodoro Clock~</h2>
+        <div className="break-session-container">
+          <BreakInterval
+            isPlay={this.state.isPlaying}
+            breakInterval={this.state.breakLength}
+            increaseBreak={this.handleIncreaseBreak}
+            decreaseBreak={this.handleDecreaseBreak}
+          />
+          <SessionLength
+            isPlay={this.state.isPlaying}
+            sessionLength={this.state.sessionLength}
+            increaseSession={this.handleIncreaseSession}
+            decreaseSession={this.handleDecreaseSession}
+          />
+        </div>
         <div className="timer-container">
           <h3 id="timer-label">
-            {this.state.isSession === true ? "Session" : "Break"}
+            {this.state.isSession === true ? this.state.currentTime : "Break"}
           </h3>
-          <span id="time-left">{this.props.timerMinute}</span>
-          <span>:</span>
-          <span>
-            {this.state.timerSecond === 0
-              ? "00"
-              : this.state.timerSecond < 10
-              ? "0" + this.state.timerSecond
-              : this.state.timerSecond}
+          <span id="time-left">
+            {this.handleUpdateTimerMinSec(this.state.timerCount)}
+            {this.handleSwitch}
+            {this.onResetTimer}
+            {this.onPlayStopTimer}
           </span>
         </div>
         <div className="timer-actions">
           <button id="start_stop" onClick={this.onPlay}>
-            Play
+            <i
+              className={`fas fa-${this.state.isPlaying ? "pause" : "play"}`}
+            ></i>
           </button>
-          <button onClick={this.onStop}>Stop</button>
           <button id="reset" onClick={this.onReset}>
-            Refresh
+            <i className="fas fa-sync-alt"></i>
           </button>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import Timer from "./Timer";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Timer />
   </React.StrictMode>,
   document.getElementById("root")
 );
